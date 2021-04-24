f34549ba48732c8d4d5acecc6832d5aebb87aef6
Constantine M Kolpashnikov <const2002@yandex.ru>
2021-04-24 03:51:51 +0300

    Первая попытка

---
+++ scripts/scr.js
@@ -0,0 +1,32 @@
+
+/* ПЕРЕМЕННЫЕ */
+let editButton = document.querySelector('.profile__edit-button');
+let closeButton = document.querySelector('.popup__close-button');
+let popup = document.querySelector('.popup');
+let title = document.querySelector('.profile__title');
+let subtitle = document.querySelector('.profile__subtitle');
+
+let formElement = document.querySelector('.popup__container');
+let nameInput = formElement.querySelector('#nameInput');
+let jobInput = formElement.querySelector('#jobInput');
+
+/* ФУНКЦИИ */
+function popupOpen () {
+  popup.classList.add('popup_opened')
+}
+
+function popupClose () {
+  popup.classList.remove('popup_opened')
+}
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    title.textContent = nameInput.value;
+    subtitle.textContent = jobInput.value;
+    popupClose();
+}
+
+/* ОБРАБОТЧИКИ СОБЫТИЙ */
+editButton.addEventListener('click', popupOpen);
+closeButton.addEventListener('click', popupClose)
+formElement.addEventListener('submit', formSubmitHandler);
