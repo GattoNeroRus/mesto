fbf409f68311e7cc3ca6a45b4aedf9f329b9c4d9
Constantine M Kolpashnikov <const2002@yandex.ru>
2021-05-04 02:32:07 +0300

    Added functionality
    modified:   blocks/element/__hearth-icon/_active/element__hearth-icon_active.css
    modified:   blocks/element/__image/element__image.css
    new file:   blocks/element/__trash-icon/element__trash-icon.css
    modified:   blocks/element/element.css
    modified:   blocks/popup/__form-submit-button/popup__form-submit-button.css
    modified:   blocks/popup/__form-title/popup__form-title.css
    new file:   blocks/popup/__image/popup__image.css
    new file:   blocks/popup/__subtitle/popup__subtitle.css
    modified:   blocks/popup/_opened/popup_opened.css
    modified:   blocks/popup/popup.css
    new file:   images/trash-icon.svg
    modified:   index.html
    modified:   pages/index.css
    modified:   scripts/scr.js
    
    	modified:   scripts/scr.js

--- scripts/scr.js
+++ scripts/scr.js
@@ -1,34 +1,103 @@
-
 /* ПЕРЕМЕННЫЕ */
-let editButton = document.querySelector('.profile__edit-button');
-let closeButton = document.querySelector('.popup__close-button');
-let popup = document.querySelector('.popup');
-let title = document.querySelector('.profile__title');
-let subtitle = document.querySelector('.profile__subtitle');
+const elementTemplate = document.querySelector('#element-template'),
+      elementsContainer = document.querySelector('.elements'),
 
-let formElement = document.querySelector('.popup__container');
-let nameInput = formElement.querySelector('#nameInput');
-let jobInput = formElement.querySelector('#jobInput');
+      profileEditPopup = document.querySelector('#profileEdit'),
+      addElementPopup = document.querySelector('#addElement'),
+      imageZoom = document.querySelector('#imageZoom'),
 
-/* ФУНКЦИИ */
-function popupOpen () {
-  nameInput.value = title.textContent;
-  jobInput.value = subtitle.textContent;
-  popup.classList.add('popup_opened');
+      addButton = document.querySelector('.profile__add-button'),
+
+      profileEditButton = document.querySelector('.profile__edit-button'),
+      profileNameInput = document.querySelector('#nameInput'),
+      profileJobInput = document.querySelector('#jobInput'),
+      profileTitle = document.querySelector('.profile__title'),
+      profileSubtitle = document.querySelector('.profile__subtitle');
+
+
+/* МАССИВ С КАРТОЧКАМИ */
+const initialCards = [
+  {
+    name: 'Элиста',
+    link: './images/elista.jpg'
+  },
+  {
+    name: 'Город столиц',
+    link: './images/city-of-capitals.jpg'
+  },
+  {
+    name: 'Церковь в Ростове',
+    link: './images/church-in-rostow.jpg'
+  },
+  {
+    name: 'Смоленск',
+    link: './images/smolensk.jpg'
+  },
+  {
+    name: 'Дворцовая площадь',
+    link: './images/palace-square.jpg'
+  },
+  {
+    name: 'Московский Кремль',
+    link: './images/kremlin.jpg'
+  }
+];
+
+
+/* УНИВЕРСАЛЬНЫЕ ФУНКЦИИ */
+function openPopup (popupName) {
+  popupName.classList.add('popup_opened');
 }
 
-function popupClose () {
-  popup.classList.remove('popup_opened');
+function closePopup (popupName) {
+  popupName.classList.remove('popup_opened');
 }
 
-function formSubmitHandler (evt) {
+function openImageZoom (e) {
+  const image = imageZoom.querySelector('.popup__image');
+
+  image.src = e.target.src;
+  image.alt, imageZoom.querySelector('.popup__subtitle').textContent = e.target.closest('.element').querySelector('.element__title').textContent;
+  openPopup(imageZoom);
+}
+
+function addElement (name, link) {
+  const newElement = elementTemplate.content.querySelector('.element').cloneNode(true),
+        image = newElement.querySelector('.element__image');
+
+  image.src = link;
+  image.alt = name;
+  image.addEventListener('click', e => openImageZoom(e));
+  newElement.querySelector('.element__title').textContent = name;
+  newElement.querySelector('.element__trash-icon').addEventListener('click', e => e.target.closest('.element').remove());
+  newElement.querySelector('.element__hearth-icon').addEventListener('click', e => e.target.classList.toggle('element__hearth-icon_active'));
+
+  elementsContainer.prepend(newElement);
+}
+
+
+/* ОБРАБОТЧИКИ СОБЫТИЙ И СМЕШАННЫЕ ФУНКЦИИ */
+initialCards.forEach(e => {addElement(e.name, e.link)});
+
+document.querySelectorAll('.popup__close-button').forEach(e => e.addEventListener('click', e => closePopup(e.target.closest('.popup'))));
+
+addButton.addEventListener('click', () => openPopup(addElementPopup)); // Не совсем понимаю, почему, но правильно работает только так
+
+profileEditButton.addEventListener('click', () => {
+  profileNameInput.value = profileTitle.textContent;
+  profileJobInput.value = profileSubtitle.textContent;
+  openPopup(profileEditPopup);
+});
+
+profileEditPopup.addEventListener('submit', evt => {
   evt.preventDefault();
-  title.textContent = nameInput.value;
-  subtitle.textContent = jobInput.value;
-  popupClose();
-}
+  profileTitle.textContent = profileNameInput.value;
+  profileSubtitle.textContent = profileJobInput.value;
+  closePopup(profileEditPopup);
+});
 
-/* ОБРАБОТЧИКИ СОБЫТИЙ */
-editButton.addEventListener('click', popupOpen);
-closeButton.addEventListener('click', popupClose)
-formElement.addEventListener('submit', formSubmitHandler);
+addElementPopup.addEventListener('submit', evt => {
+  evt.preventDefault();
+  addElement(addElementPopup.querySelector('#elementTitle').value, addElementPopup.querySelector('#elementImage').value);
+  closePopup(addElementPopup);
+});
